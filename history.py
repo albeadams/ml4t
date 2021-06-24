f8e72092c48385dc2aa74058e5c7b27037859cb6
Adams <albeadam@helenaagri.com>
2021-06-23 21:26:37 -0500

    changed to history

---
+++ history.py
@@ -0,0 +1,65 @@
+import pandas as pd
+
+from portfolios import *
+
+class History(object):
+    def __init__(self, DataStore=None, symbol=None, value='adjusted close', indicators='all'):
+        assert DataStore is not None, "supply a DataStore"
+        assert symbol is not None, "specify a symbol"
+        assert (indicators != 'all' and isinstance(indicators, list)), "supply indicators as list"
+        self.symbol = symbol
+        self.value = value
+        self.indicators = indicators
+        self.DataStore = DataStore
+
+        if not isinstance(symbol, list):
+            symbol = symbol.split(',')
+            symbol = [x.strip() for x in symbol]
+        if len(symbol) > 1:
+            print('use build_multiple_state(list) when specifying multiple symbols')
+            return
+
+        symbol = symbol[0]
+        
+        # get value (open, close, etc.)
+        x,y = DataStore.read(symbol + '_VAL')
+        values = []
+        for key, val in x.items():
+            for k, v in val.items():
+                if value in k:
+                    values.append((key, v))
+
+        df = pd.DataFrame(values, columns = ['Date',value])
+        df['Date'] = pd.to_datetime(df['Date'])
+        df.set_index('Date', inplace=True)
+        df = df.sort_index()
+        self.prices = df
+
+        # add alpha values
+        symbols = []
+        if indicators == 'all':
+            symbols.append(DataStore.list_indicator_symbols(sym=symbol))
+            symbols = [item for sublist in symbols for item in sublist]
+        else:
+            for ind in indicators:
+                symbols.append(symbol + '_' + ind + '_daily')  # daily only for now
+
+        for symbol in symbols:
+            x = DataStore.read(symbol)
+            df2 = pd.DataFrame.from_dict(x[0], orient='index').astype(float)
+            df = df.merge(df2, how='inner', left_index=True, right_index=True)
+
+        df = df.fillna(method='ffill')
+        df = df.sort_index()
+        df1 = df.iloc[:,0].to_frame() # note: joining of dataframes not currently used
+        del df1
+        df2 = df.iloc[:, 1:]/df.iloc[0,1:]
+
+        self.indicators = df2
+        
+        print('Available to use:\n  <classname>.prices\n  <classname>.indicators')
+        
+    
+    @staticmethod
+    def normalize_indicators(df):
+        return df.iloc[:,1:]/df.iloc[0,1:]
